19898b2ba7dc549e4fe8a07aa29fe507383e7f6a
Gaganrajput0912 <Gaganrajput717114@gmail.com>
2023-03-29 20:23:50 +0530

    Sign up

--- mailboxclient/src/App.js
+++ mailboxclient/src/App.js
@@ -1,7 +1,15 @@
 import "./App.css";
+import AllRoutes from "./components/Routes/AllRoutes";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <div></div>;
+  return (
+    <div className="App">
+      <Header />
+
+      <AllRoutes />
+    </div>
+  );
 }
 
 export default App;
